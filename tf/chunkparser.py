5a4a6c734bbf8178bd084625b954dcef72a35e4c
Agusanso7 <38085938+Agusanso7@users.noreply.github.com>
2024-03-17 23:07:02 -0300

    Update chunkparser.py

--- tf/chunkparser.py
+++ tf/chunkparser.py
@@ -73,7 +73,7 @@ V5_VERSION = struct.pack('i', 5)
 CLASSICAL_INPUT = struct.pack('i', 1)
 V4_VERSION = struct.pack('i', 4)
 V3_VERSION = struct.pack('i', 3)
-V6_STRUCT_STRING = '4si7432s832sBBBBBBBbfffffffffffffffIHH4H'
+V6_STRUCT_STRING = '288s224s' #'4si7432s832sBBBBBBBbfffffffffffffffIHH4H'
 V5_STRUCT_STRING = '4si7432s832sBBBBBBBbfffffff'
 V4_STRUCT_STRING = '4s7432s832sBBBBBBBbffff'
 V3_STRUCT_STRING = '4s7432s832sBBBBBBBb'
@@ -298,12 +298,7 @@ class ChunkParserInner:
         """
         # unpack the V6 content from raw byte array, arbitrarily chose 4 2-byte values
         # for the 8 "reserved" bytes
-        (ver, input_format, probs, planes, us_ooo, us_oo, them_ooo, them_oo,
-         stm, rule50_count, invariance_info, dep_result, root_q, best_q,
-         root_d, best_d, root_m, best_m, plies_left, result_q, result_d,
-         played_q, played_d, played_m, orig_q, orig_d, orig_m, visits,
-         played_idx, best_idx, reserved1, reserved2, reserved3,
-         reserved4) = self.v6_struct.unpack(content)
+        (planes, move) = self.v6_struct.unpack(content)
         """
         v5 struct format was (8308 bytes total)
             int32 version (4 bytes)
@@ -326,81 +321,19 @@ class ChunkParserInner:
             float32 best_m (4 bytes)
             float32 plies_left (4 bytes)
         """
-        # v3/4 data sometimes has a useful value in dep_ply_count (now invariance_info),
-        # so copy that over if the new ply_count is not populated.
-        if plies_left == 0:
-            plies_left = invariance_info
-        plies_left = struct.pack('f', plies_left)
-
-        assert input_format == self.expected_input_format
 
         # Unpack bit planes and cast to 32 bit float
-        planes = np.unpackbits(np.frombuffer(planes, dtype=np.uint8)).astype(
-            np.float32)
-        rule50_divisor = 99.0
-        if input_format > 3:
-            rule50_divisor = 100.0
-        rule50_plane = struct.pack('f', rule50_count / rule50_divisor) * 64
-
-        if input_format == 1:
-            middle_planes = self.flat_planes[us_ooo] + \
-                            self.flat_planes[us_oo] + \
-                            self.flat_planes[them_ooo] + \
-                            self.flat_planes[them_oo] + \
-                            self.flat_planes[stm]
-        elif input_format == 2:
-            # Each inner array has to be reversed as these fields are in opposite endian to the planes data.
-            them_ooo_bytes = reverse_expand_bits(them_ooo)
-            us_ooo_bytes = reverse_expand_bits(us_ooo)
-            them_oo_bytes = reverse_expand_bits(them_oo)
-            us_oo_bytes = reverse_expand_bits(us_oo)
-            middle_planes = us_ooo_bytes + (6*8*4) * b'\x00' + them_ooo_bytes + \
-                            us_oo_bytes + (6*8*4) * b'\x00' + them_oo_bytes + \
-                            self.flat_planes[0] + \
-                            self.flat_planes[0] + \
-                            self.flat_planes[stm]
-        elif input_format == 3 or input_format == 4 or input_format == 132 or input_format == 5 or input_format == 133:
-            # Each inner array has to be reversed as these fields are in opposite endian to the planes data.
-            them_ooo_bytes = reverse_expand_bits(them_ooo)
-            us_ooo_bytes = reverse_expand_bits(us_ooo)
-            them_oo_bytes = reverse_expand_bits(them_oo)
-            us_oo_bytes = reverse_expand_bits(us_oo)
-            enpassant_bytes = reverse_expand_bits(stm)
-            middle_planes = us_ooo_bytes + (6*8*4) * b'\x00' + them_ooo_bytes + \
-                            us_oo_bytes + (6*8*4) * b'\x00' + them_oo_bytes + \
-                            self.flat_planes[0] + \
-                            self.flat_planes[0] + \
-                            (7*8*4) * b'\x00' + enpassant_bytes
+        planes = np.unpackbits(np.frombuffer(planes, dtype=np.uint8)).astype(np.float32)
+        
+        probs = np.unpackbits(np.frombuffer(move, dtype=np.uint8)).astype(np.float32)
 
         # Concatenate all byteplanes. Make the last plane all 1's so the NN can
         # detect edges of the board more easily
-        aux_plus_6_plane = self.flat_planes[0]
-        if (input_format == 132
-                or input_format == 133) and invariance_info >= 128:
-            aux_plus_6_plane = self.flat_planes[1]
-        planes = planes.tobytes() + \
-                 middle_planes + \
-                 rule50_plane + \
-                 aux_plus_6_plane + \
-                 self.flat_planes[1]
+        #planes = planes.tobytes() + self.flat_planes[1]
 
-        assert len(planes) == ((8 * 13 * 1 + 8 * 1 * 1) * 8 * 8 * 4)
+        #assert len(planes) == ((8 * 13 * 1 + 8 * 1 * 1) * 8 * 8 * 4)
 
-        if ver == V6_VERSION:
-            winner = struct.pack('fff', 0.5 * (1.0 - result_d + result_q),
-                                 result_d, 0.5 * (1.0 - result_d - result_q))
-        else:
-            dep_result = float(dep_result)
-            assert dep_result == 1.0 or dep_result == -1.0 or dep_result == 0.0
-            winner = struct.pack('fff', dep_result == 1.0, dep_result == 0.0,
-                                 dep_result == -1.0)
-
-        best_q_w = 0.5 * (1.0 - best_d + best_q)
-        best_q_l = 0.5 * (1.0 - best_d - best_q)
-        assert -1.0 <= best_q <= 1.0 and 0.0 <= best_d <= 1.0
-        best_q = struct.pack('fff', best_q_w, best_d, best_q_l)
-
-        return (planes, probs, winner, best_q, plies_left)
+        return (planes, probs)
 
     def sample_record(self, chunkdata):
         """
@@ -408,17 +341,7 @@ class ChunkParserInner:
         Downsampling to avoid highly correlated positions skips most records, and 
         diff focus may also skip some records.
         """
-        version = chunkdata[0:4]
-        if version == V6_VERSION:
-            record_size = self.v6_struct.size
-        elif version == V5_VERSION:
-            record_size = self.v5_struct.size
-        elif version == V4_VERSION:
-            record_size = self.v4_struct.size
-        elif version == V3_VERSION:
-            record_size = self.v3_struct.size
-        else:
-            return
+        record_size = self.v6_struct.size
 
         for i in range(0, len(chunkdata), record_size):
             if self.sample > 1:
@@ -427,56 +350,13 @@ class ChunkParserInner:
                     continue  # Skip this record.
 
             record = chunkdata[i:i + record_size]
-            # for earlier versions, append fake bytes to record to maintain size
-            if version == V3_VERSION:
-                # add 16 bytes of fake root_q, best_q, root_d, best_d to match V4 format
-                record += 16 * b'\x00'
-            if version == V3_VERSION or version == V4_VERSION:
-                # add 12 bytes of fake root_m, best_m, plies_left to match V5 format
-                record += 12 * b'\x00'
-                # insert 4 bytes of classical input format tag to match v5 format
-                record = record[:4] + CLASSICAL_INPUT + record[4:]
-            if version == V3_VERSION or version == V4_VERSION or version == V5_VERSION:
-                # add 48 byes of fake result_q, result_d etc
-                record += 48 * b'\x00'
-
-            if version == V6_VERSION:
-                # diff focus code, peek at best_q, orig_q and pol_kld from record (unpacks as tuple with one item)
-                best_q = struct.unpack('f', record[8284:8288])[0]
-                orig_q = struct.unpack('f', record[8328:8332])[0]
-                pol_kld = struct.unpack('f', record[8348:8352])[0]
-
-                # if orig_q is NaN or pol_kld is 0, accept, else accept based on diff focus
-                if not np.isnan(orig_q) and pol_kld > 0:
-                    diff_q = abs(best_q - orig_q)
-                    q_weight = self.diff_focus_q_weight
-                    pol_scale = self.diff_focus_pol_scale
-                    total = (q_weight * diff_q + pol_kld) / (q_weight +
-                                                             pol_scale)
-                    thresh_p = self.diff_focus_min + self.diff_focus_slope * total
-                    if thresh_p < 1.0 and random.random() > thresh_p:
-                        continue
-
             yield record
 
     def single_file_gen(self, filename):
         try:
             with gzip.open(filename, 'rb') as chunk_file:
-                version = chunk_file.read(4)
-                chunk_file.seek(0)
-                if version == V6_VERSION:
-                    record_size = self.v6_struct.size
-                elif version == V5_VERSION:
-                    record_size = self.v5_struct.size
-                elif version == V4_VERSION:
-                    record_size = self.v4_struct.size
-                elif version == V3_VERSION:
-                    record_size = self.v3_struct.size
-                else:
-                    print('Unknown version {} in file {}'.format(
-                        version, filename))
-                    return
                 while True:
+                    record_size = self.v6_struct.size
                     chunkdata = chunk_file.read(256 * record_size)
                     if len(chunkdata) == 0:
                         break
@@ -551,16 +431,14 @@ class ChunkParserInner:
             s = list(itertools.islice(gen, self.batch_size))
             if not len(s) or (not allow_partial and len(s) != self.batch_size):
                 return
-            yield (b''.join([x[0] for x in s]), b''.join([x[1] for x in s]),
-                   b''.join([x[2] for x in s]), b''.join([x[3] for x in s]),
-                   b''.join([x[4] for x in s]))
+            yield (b''.join([x[0] for x in s]), b''.join([x[1] for x in s]))
 
     def parse(self):
         """
         Read data from child workers and yield batches of unpacked records
         """
         gen = self.v6_gen()  # read from workers
-        gen = self.tuple_gen(gen)  # convert v6->tuple
+        gen = self.tuple_gen(gen)  # convert v6->tuple # TODO: change this !!
         gen = self.batch_gen(gen)  # assemble into batches
         for b in gen:
             yield b
