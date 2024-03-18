8721dad79e4d19ce59cd2f6b82aa42d734bdd19d
Agusanso7 <38085938+Agusanso7@users.noreply.github.com>
2024-03-17 23:07:19 -0300

    Update chunkparsefunc.py

--- tf/chunkparsefunc.py
+++ tf/chunkparsefunc.py
@@ -17,21 +17,16 @@
 #    along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
 import tensorflow as tf
 
+HISTORY = 2
 
-def parse_function(planes, probs, winner, q, plies_left):
+def parse_function(planes, probs):
     """
     Convert unpacked record batches to tensors for tensorflow training
     """
     planes = tf.io.decode_raw(planes, tf.float32)
     probs = tf.io.decode_raw(probs, tf.float32)
-    winner = tf.io.decode_raw(winner, tf.float32)
-    q = tf.io.decode_raw(q, tf.float32)
-    plies_left = tf.io.decode_raw(plies_left, tf.float32)
 
-    planes = tf.reshape(planes, (-1, 112, 8, 8))
-    probs = tf.reshape(probs, (-1, 1858))
-    winner = tf.reshape(winner, (-1, 3))
-    q = tf.reshape(q, (-1, 3))
-    plies_left = tf.reshape(plies_left, (-1, 1))
+    planes = tf.reshape(planes, (-1, 18*HISTORY, 8, 8))
+    probs = tf.reshape(probs, (-1, 1792))
 
-    return (planes, probs, winner, q, plies_left)
+    return (planes, probs)
